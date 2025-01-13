fb1ce1d0d6656f2fe83c2dfc89412b2db45636ef
pranshusingh2 <prashantsingh1134570@gmail.com>
2025-01-13 11:37:55 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "Hde632_62skir_8bot"
-BOT_TOKEN = "7576691148:AAH5k_diAzJJlgyMh3q9zl3MBaVsrWv6ALE"
+BOT_NAME = " g"
+BOT_TOKEN = "7576691148: "
 API_ID = 14578218
 API_HASH = "1910055ec43e4b46efeb768a82bdaf68"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
